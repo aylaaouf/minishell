fe5329de6e20d6d9e070da967de89660900e7aa2
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-05-08 19:28:40 +0100

    add: beta echo

--- main.c
+++ main.c
@@ -6,11 +6,31 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 19:15:16 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/05/07 16:46:46 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/05/08 19:16:58 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_echo(char *input)
+{
+	int	i;
+	
+	i = 5;
+	// while (args[2][i])
+	// {
+	// 	if (args[2][i] == '-' && args[2][i + 1] == 'n')
+	// }
+	while (input[i])
+	{
+		// if (input[i] ==  '-' && input[i + 1] == 'n')
+		// 	printf("%s", input[i]);
+		// else
+		printf("%c", input[i]);
+		i++;
+	}
+	printf("\n");
+}
 
 int	main(int argc, char *argv[], char **env)
 {
@@ -31,6 +51,8 @@ int	main(int argc, char *argv[], char **env)
 		}
 		if (!strcmp(input, "env"))
 			print_env(my_env);
+		if (!strncmp(input, "echo", 4))
+			ft_echo(input);
 		if (input)
 			add_history(input);
 	}
