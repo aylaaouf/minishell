5345b7cfa018c96eeb4ddfab21a3c1dc75c7323b
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-05-18 19:24:23 +0100

    excute cmnd without path

--- shell.c
+++ shell.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 17:07:09 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/05/18 18:57:27 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/05/18 19:23:28 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,7 @@ int    shell(t_command *cmnd, t_env *env)
     pid_t child_pid;
     int status;
     char **args;
+    char *merged_cmnd;
 
     if (!cmnd || !cmnd->args || !cmnd->args[0])
         return (1);
@@ -84,7 +85,11 @@ int    shell(t_command *cmnd, t_env *env)
     }
     else if (child_pid == 0)
     {
-        if (execve(cmnd->args[0], cmnd->args, args) == -1)
+        if (strncmp(cmnd->args[0], "/bin/", 5))
+            merged_cmnd = ft_strjoin("/bin/", cmnd->args[0]);
+        else
+            merged_cmnd = ft_strdup(cmnd->args[0]);   
+        if (execve(merged_cmnd, cmnd->args, args) == -1)
         {
             perror("Couldn't execute");
             exit(7);
