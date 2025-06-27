f04af7f36fb861856dd5884cb22e775255e2a2a5
aelasefa <aymanelasefar1337@gmail.com>
2025-06-27 20:59:48 +0100

    fix gc in heredoc

--- heredoc.c
+++ heredoc.c
@@ -23,20 +23,19 @@ int is_quoted(const char *s)
             (s[0] == '"' && s[len - 1] == '"'));
 }
 
-char *strip_quotes(const char *s)
+char *strip_quotes(t_gc *gc, const char *s)
 {
     size_t len = ft_strlen(s);
     if (is_quoted(s))
-        return ft_substr(s, 1, len - 2);
-    return ft_strdup(s);
+        return gc_substr(gc, s, 1, len - 2);
+    return gc_strdup(gc, s);
 }
 
-char *expand_if_needed(char *line, t_env *env, int do_expand, int last_exit_status)
+char *expand_if_needed(t_gc *gc, char *line, t_env *env, int do_expand, int last_exit_status)
 {
     if (!do_expand)
         return line;
-    char *expanded = expand_env(line, env, last_exit_status);
-    free(line);
+    char *expanded = expand_env(gc, line, env, last_exit_status);
     return expanded;
 }
 
@@ -46,7 +45,7 @@ static void heredoc_child_handler(int sig)
     exit(130);
 }
 
-static int heredoc_pipe(const char *raw_delim, t_env *env, int last_exit_status)
+static int heredoc_pipe(t_gc *gc, const char *raw_delim, t_env *env, int last_exit_status)
 {
     int pipefd[2];
     char *line;
@@ -54,20 +53,21 @@ static int heredoc_pipe(const char *raw_delim, t_env *env, int last_exit_status)
     int pid;
 
     int do_expand = !is_quoted(raw_delim);
-    char *delimiter = strip_quotes(raw_delim);
+    char *delimiter = strip_quotes(gc, raw_delim);
 
     if (pipe(pipefd) == -1)
     {
         perror("pipe");
-        free(delimiter);
         return -1;
     }
+
     status = 0;
     pid = fork();
     if (pid == 0)
     {
         signal(SIGINT, heredoc_child_handler);
         close(pipefd[0]);
+
         while (1)
         {
             line = readline("> ");
@@ -78,31 +78,30 @@ static int heredoc_pipe(const char *raw_delim, t_env *env, int last_exit_status)
                 free(line);
                 break;
             }
-            line = expand_if_needed(line, env, do_expand, last_exit_status);
+            char *tmp = line;
+            line = expand_if_needed(gc, line, env, do_expand, last_exit_status);
+            free(tmp);
             write(pipefd[1], line, strlen(line));
             write(pipefd[1], "\n", 1);
-            free(line);
         }
-        free(delimiter);
+
         close(pipefd[1]);
         exit(0);
     }
+
     close(pipefd[1]);
     waitpid(pid, &status, 0);
-    
-    // Check if child was interrupted by Ctrl+C
+
     if (WIFEXITED(status) && WEXITSTATUS(status) == 130)
     {
         close(pipefd[0]);
-        free(delimiter);
         return -1;
     }
-    
-    free(delimiter);
+
     return pipefd[0];
 }
 
-int	process_heredocs(t_command *commands, t_env *env, int last_exit_status)
+int process_heredocs(t_gc *gc, t_command *commands, t_env *env, int last_exit_status)
 {
     t_command *cmd = commands;
     t_redirection *redir;
@@ -111,22 +110,25 @@ int	process_heredocs(t_command *commands, t_env *env, int last_exit_status)
     {
         cmd->heredoc_fd = -1;
         redir = cmd->redir;
+
         while (redir)
         {
             if (strcmp(redir->type, "<<") == 0)
             {
                 if (cmd->heredoc_fd != -1)
                     close(cmd->heredoc_fd);
-                cmd->heredoc_fd = heredoc_pipe(redir->file, env, last_exit_status);
+                cmd->heredoc_fd = heredoc_pipe(gc, redir->file, env, last_exit_status);
                 if (cmd->heredoc_fd == -1)
-                {
                     return -1;
-                }
                 cmd->has_heredoc = 1;
             }
             redir = redir->next;
         }
+
         cmd = cmd->next;
     }
-    return (0);
+
+    return 0;
 }
+
+
