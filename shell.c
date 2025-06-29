81e0de539386c0189c8b13b78d3dd8e3a088fc68
aelasefa <aymanelasefar1337@gmail.com>
2025-06-29 10:36:47 +0100

    exit status is fixed

--- shell.c
+++ shell.c
@@ -12,22 +12,22 @@
 
 #include "minishell.h"
 
-char    **list_to_array(t_env *env)
+char **list_to_array(t_env *env)
 {
-    int i;
+    int i = 0;
     char **args;
-    t_env *tmp;
+    t_env *tmp = env;
 
-    i = 0;
-    tmp = env;
     while (tmp)
     {
         i++;
         tmp = tmp->next;
     }
+
     args = malloc((i + 1) * sizeof(char *));
     if (!args)
-        return (NULL);
+        return NULL;
+
     tmp = env;
     i = 0;
     while (tmp)
@@ -37,27 +37,31 @@ char    **list_to_array(t_env *env)
         tmp = tmp->next;
     }
     args[i] = NULL;
-    return (args);
+    return args;
 }
 
-char    *find_cmnd_path(t_gc *gc, char *cmnd, t_env *env)
+char *find_cmnd_path(t_gc *gc, char *cmnd, t_env *env)
 {
     char *path_env;
     char **path;
     char *full_path;
-    int i;
+    int i = 0;
 
     if (cmnd[0] == '/' || cmnd[0] == '.')
     {
         if (!access(cmnd, X_OK))
-            return (gc_strdup(gc, cmnd));
-        return (NULL);
+            return gc_strdup(gc, cmnd);
+        return NULL;
     }
+
     path_env = get_env_value(env, "PATH");
     if (!path_env || !cmnd)
-        return (NULL);
+        return NULL;
+
     path = ft_split(path_env, ':');
-    i = 0;
+    if (!path)
+        return NULL;
+
     while (path[i])
     {
         full_path = ft_strjoin(path[i], "/");
@@ -65,16 +69,17 @@ char    *find_cmnd_path(t_gc *gc, char *cmnd, t_env *env)
         if (!access(full_path, X_OK))
         {
             free_2d_array(path);
-            return (full_path);
+            return full_path;
         }
         free(full_path);
         i++;
     }
+
     free_2d_array(path);
-    return (NULL);
+    return NULL;
 }
 
-int    shell(t_gc *gc, t_command *cmnd, t_env *env)
+int shell(t_gc *gc, t_command *cmnd, t_env *env)
 {
     pid_t child_pid;
     int status;
@@ -82,19 +87,23 @@ int    shell(t_gc *gc, t_command *cmnd, t_env *env)
     char *path;
 
     if (!cmnd || !cmnd->args || !cmnd->args[0])
-        return (1);
+        return 1;
+
     path = find_cmnd_path(gc, cmnd->args[0], env);
     if (!path)
     {
         printf("%s: command not found\n", cmnd->args[0]);
-        return (1);
+        g_last_exit_status = 127;
+        return 127;
     }
-    child_pid = fork();
+
     args = list_to_array(env);
+    child_pid = fork();
     if (child_pid == -1)
     {
         perror("fork");
-        exit(41);
+        g_last_exit_status = 1;
+        return 1;
     }
     else if (child_pid == 0)
     {
@@ -106,11 +115,9 @@ int    shell(t_gc *gc, t_command *cmnd, t_env *env)
             close(cmnd->heredoc_fd);
         }
         handle_redirection(cmnd, -1);
-        if (execve(path, cmnd->args, args) == -1)
-        {
-            perror("Couldn't execute");
-            exit(7);
-        }
+        execve(path, cmnd->args, args);
+        perror("Couldn't execute");
+        exit(127);
     }
     else
     {
@@ -118,6 +125,12 @@ int    shell(t_gc *gc, t_command *cmnd, t_env *env)
         free_2d_array(args);
         if (cmnd->heredoc_fd != -1)
             close(cmnd->heredoc_fd);
+
+        if (WIFEXITED(status))
+            g_last_exit_status = WEXITSTATUS(status);
+        else if (WIFSIGNALED(status))
+            g_last_exit_status = 128 + WTERMSIG(status);
     }
-    return (0);
+
+    return 0;
 }
