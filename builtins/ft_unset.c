27b95eeaa7317c0e7d9bca68bbdf5d65e7b215cc
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-07-07 18:31:07 +0100

    fix style of unset and pwd

--- builtins/ft_unset.c
+++ builtins/ft_unset.c
@@ -1,16 +1,39 @@
-/*************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: aylaaouf <aylaaouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 22:56:15 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/06/03 23:33:15 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/07/07 18:28:35 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	free_env_node(t_env *node)
+{
+	if (node)
+	{
+		free(node->key);
+		if (node->value)
+			free(node->value);
+		free(node);
+	}
+}
+
+void	helper(t_env **env, char **args, int *i)
+{
+	t_env	*deleted;
+
+	if (*env && (*env)->key && ft_strcmp(args[*i], (*env)->key) == 0)
+	{
+		deleted = *env;
+		*env = (*env)->next;
+		free_env_node(deleted);
+	}
+}
 
 void	ft_unset(char **args, t_env **env)
 {
@@ -22,15 +45,7 @@ void	ft_unset(char **args, t_env **env)
 	g_last_exit_status = 1;
 	while (args[i])
 	{
-		if (*env && (*env)->key && ft_strcmp(args[i], (*env)->key) == 0)
-		{
-			deleted = *env;
-			*env = (*env)->next;
-			free(deleted->key);
-			if (deleted->value)
-				free(deleted->value);
-			free(deleted);
-		}
+		helper(env, args, &i);
 		curr = *env;
 		while (curr && curr->next)
 		{
@@ -38,10 +53,7 @@ void	ft_unset(char **args, t_env **env)
 			{
 				deleted = curr->next;
 				curr->next = curr->next->next;
-				free(deleted->key);
-				if (deleted->value)
-					free(deleted->value);
-				free(deleted);
+				free_env_node(deleted);
 				break ;
 			}
 			curr = curr->next;
