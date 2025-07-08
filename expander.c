eba3ede0f754536e479fea1da287f6233141c687
aelasefa <aymanelasefar1337@gmail.com>
2025-07-08 16:07:41 +0100

    no chnage

--- expander.c
+++ expander.c
@@ -6,7 +6,7 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:06:04 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/06/23 13:57:48 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/07/08 15:22:39 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,27 @@ static char	*handle_quotes(char *str, size_t *i, char *quote)
 	return (NULL);
 }
 
+static char	*expand_token_value_no_squote(t_gc *gc, char *str, t_env *env)
+{
+	size_t	i;
+	char	*result;
+	char	*expansion;
+
+	i = 0;
+	result = gc_strdup(gc, "");
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			expansion = handle_dollar_expansion(gc, str, &i, env);
+			result = gc_strjoin_free_a(gc, result, expansion);
+		}
+		else
+			result = process_character(gc, result, str, &i);
+	}
+	return (result);
+}
+
 char	*expand_token_value(t_gc *gc, char *str, t_env *env)
 {
 	size_t	i;
@@ -91,6 +112,16 @@ char	*expand_token_value(t_gc *gc, char *str, t_env *env)
 	return (result);
 }
 
+static int	should_expand_token(t_token *token)
+{
+	if (token->type == TOKEN_SQUOTE)
+		return (0);
+	if ((token->type == TOKEN_WORD || token->type == TOKEN_DQUOTE)
+		&& strchr(token->value, '$'))
+		return (1);
+	return (0);
+}
+
 void	expander(t_gc *gc, t_token *tokens, t_env *env)
 {
 	while (tokens)
@@ -100,10 +131,13 @@ void	expander(t_gc *gc, t_token *tokens, t_env *env)
 			tokens = tokens->next->next;
 			continue ;
 		}
-		if ((tokens->type == TOKEN_WORD || tokens->type == TOKEN_DQUOTE)
-			&& strchr(tokens->value, '$'))
+		if (should_expand_token(tokens))
 		{
-			tokens->value = expand_token_value(gc, tokens->value, env);
+			if (tokens->type == TOKEN_DQUOTE)
+				tokens->value = expand_token_value(gc, tokens->value, env);
+			else
+				tokens->value = expand_token_value_no_squote(gc,
+						tokens->value, env);
 		}
 		tokens = tokens->next;
 	}
--- quote_management.c
+++ quote_management.c
@@ -6,18 +6,19 @@
 /*   By: ayelasef <ayelasef@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:07:32 by ayelasef          #+#    #+#             */
-/*   Updated: 2025/05/07 15:12:09 by ayelasef         ###   ########.fr       */
+/*   Updated: 2025/07/08 13:28:14 by ayelasef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*strip_quotes(t_gc *gc, const char *str)
+static char	*ft_strip_quotes(t_gc *gc, const char *str)
 {
 	size_t	len;
 
 	len = ft_strlen(str);
-	if (len >= 2 && ((str[0] == '\'' && str[len - 1] == '\'') || (str[0] == '"' && str[len - 1] == '"')))
+	if (len >= 2 && ((str[0] == '\'' && str[len - 1] == '\'') || (str[0] == '"'
+				&& str[len - 1] == '"')))
 		return (gc_strndup(gc, str + 1, len - 2));
 	return (gc_strdup(gc, str));
 }
@@ -31,10 +32,10 @@ void	quote_management(t_gc *gc, t_token *tokens)
 	prev = NULL;
 	while (cur)
 	{
-		if ((cur->type == TOKEN_SQUOTE || cur->type == TOKEN_DQUOTE)
-			&& !(prev && prev->type == TOKEN_HEREDOC))
+		if ((cur->type == TOKEN_SQUOTE || cur->type == TOKEN_DQUOTE) && !(prev
+				&& prev->type == TOKEN_HEREDOC))
 		{
-			cur->value = strip_quotes(gc, cur->value);
+			cur->value = ft_strip_quotes(gc, cur->value);
 		}
 		prev = cur;
 		cur = cur->next;
